50c0da68e37e786262895e0d11ed2b91a5ba2f49
nico.lehmann <nico@fastlane.de>
2019-02-07 00:45:41 +0100

    update gitignore and new child-theme

---
+++ source/Application/views/fastlane/theme.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Theme Information
+*/
+
+$aTheme = array(
+    'id'            =>  'fastlane',
+    'title'         =>  'The New Shit',
+    'description'   =>  'the is a new theme for fastlane shop',
+    'thumbnail'     =>  'theme.jpg',
+    'version'       =>  '1.0.0',
+    'author'        =>  'fastlane',
+    'parentTheme'   =>  'wave',
+    'parentVersions' =>  array('1.0.1'),
+);
